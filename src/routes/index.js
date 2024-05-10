8fc3450cfb12ea89c509e5c2494cf773668a8aef
Nirob Hasan <nirob427417@gmail.com>
2024-05-10 12:16:29 +0600

    article

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,44 +1,38 @@
-const router = require("express").Router();
-const authController = require("../controller/auth");
-const userController = require("../controller/user");
-const authenticate = require("../middleware/authenticate");
-const authorize = require("../middleware/authorize");
+const router = require('express').Router()
+const authController = require('../controller/auth')
+const userController = require('../controller/user')
+const articleController = require('../controller/article')
+const authenticate = require('../middleware/authenticate')
+const authorize = require('../middleware/authorize')
+const ownership = require('../middleware/ownership')
 
 //Auth routes
 router
-  .post("/api/v1/auth/register", authController.register)
-  .post("/api/v1/auth/login", authController.login);
+  .post('/api/v1/auth/register', authController.register)
+  .post('/api/v1/auth/login', authController.login)
 
 // User routes
 router
-  .route("/api/v1/users")
-  .get(authenticate, authorize(["admin"]), userController.findAllItems)
-  .post(authenticate, authorize(["admin"]), userController.create);
+  .route('/api/v1/users')
+  .get(authenticate, authorize(['admin']), userController.findAllItems)
+  .post(authenticate, authorize(['admin']), userController.create)
 
 // Article routes
-// router
-//   .route("/api/v1/articles")
-//   .get(articleController.findAllItems)
-//   .post(authenticate, authorize(["admin", "user"]), articleController.create);
+router
+  .route('/api/v1/articles')
+  .get(articleController.findAllItems)
+  .post(authenticate, authorize(['admin', 'user']), articleController.create)
 
-// router
-//   .route('/api/v1/articles/:id')
-//   .get(articleController.findSingleItem)
-//   .put(authenticate, authorize(['user', 'admin']), articleController.updateItem)
-//   .patch(
-//     authenticate,
-//     authorize(['user', 'admin']),
-//     articleController.updateItemPatch
-//   )
-//   .delete(
-//     authenticate,
-//     authorize(['admin', 'user']),
-//     ownership('Article'),
-//     articleController.removeItem
-//   );
+router
+  .route('/api/v1/articles/:id')
+  .get(articleController.findSingleItem)
+  .put(authenticate, authorize(['user', 'admin']), articleController.updateItem)
+  .patch(authenticate, authorize(['user', 'admin']), articleController.updateItemPatch)
+  .delete(
+    authenticate,
+    authorize(['admin', 'user']),
+    ownership('Article'),
+    articleController.removeItem,
+  )
 
-// router
-//   .route('/api/v2/articles/:id')
-//   .patch(authenticate, articleControllerV2.updateItemPatch);
-
-module.exports = router;
+module.exports = router
